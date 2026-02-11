47f8a60f9b052fdc0226ecd681de139668068d2f
Youngjin-Luke-Kim <ykim9554@usc.edu>
2026-02-11 17:34:25 +0000

    Deploying to gh-pages from @ Youngjin-Luke-Kim/Youngjin-Luke-Kim.github.io@fba3a27bbc988a7c2facb24768357210d3de4a95 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "This is a description of your CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
